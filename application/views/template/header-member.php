241aea7480023ec72be869cd4f13d30692584662
rainhanifa <vj2daworld@gmail.com>
2017-06-20 09:58:14 +0700

    remove unnecessary files

---
+++ application/views/template/header-member.php
@@ -0,0 +1,31 @@
+<!--
+    SMART MEDIA CLIENT AREA
+    Illiyin Studio
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Dashboard - Client Area</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <!--base css styles-->
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/assets/font-awesome/css/font-awesome.min.css">
+
+        
+        <!--page specific css styles-->
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/assets/data-tables/bootstrap3/dataTables.bootstrap.css" />
+
+        <!--flaty css styles-->
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/css/flaty.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/css/flaty-responsive.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets')?>/css/style.css">
+
+        <link rel="shortcut icon" href="<?php echo base_url('assets')?>/img/favicon.png">
+    </head>
+    <body>
