ec4ee740ea3c13c12d2b85542199ceaefdfa6bbf
naayunir <naayunir@gmail.com>
2017-06-20 11:22:31 +0700

    view

---
+++ application/views/store/store.php
@@ -0,0 +1,184 @@
+            <!-- BEGIN Content -->
+            <div id="main-content">
+                <!-- BEGIN Page Title -->
+                <div class="page-title">
+                    <div>
+                        <h1><i class="fa fa-shopping-cart"></i> Store</h1>
+                        <h4></h4>
+                    </div>
+                </div>
+                <!-- END Page Title -->
+
+                <!-- BEGIN Breadcrumb -->
+                <div id="breadcrumbs">
+                    <ul class="breadcrumb">
+                        <li>
+                            <i class="fa fa-home"></i>
+                            <a href="index.html">Home</a>
+                            <span class="divider"><i class="fa fa-angle-right"></i></span>
+                        </li>
+                        <li class="active">Store</li>
+                    </ul>
+                </div>
+                <!-- END Breadcrumb -->
+
+                <!-- BEGIN Main Content -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="box">
+                            <div class="box-content">
+                                <div class="clearfix">
+                                    <div class="pull-left">
+                                        <div class="btn-toolbar">
+                                            <div class="btn-group">
+                                                <select name="cat" class="chosen gallery-cat col-xs-12" data-placeholder="Filter category">
+                                                    <option value=""> </option>
+                                                    <option value="opt-1">Category-1</option>
+                                                    <option value="opt-2">Category-2</option>
+                                                    <option value="opt-3">Category-3</option>
+                                                    <option value="opt-4">Category-4</option>
+                                                    <option value="opt-5">Category-5</option>
+                                                </select>
+                                            </div>
+                                            <div class="btn-group">
+                                                <select name="sort" class="chosen gallery-sort" data-placeholder="Sort by" data-nosearch="true">
+                                                    <option value=""> </option>
+                                                    <option value="sort-1">Date</option>
+                                                    <option value="sort-2">Author</option>
+                                                    <option value="sort-3">Category</option>
+                                                </select>
+                                            </div>
+                                            <div class="btn-group">
+                                                <a href="#" class="btn btn-success show-tooltip" title="Apply filter"><i class="fa fa-check"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>
+                                <ul class="gallery">
+                                    <li>
+                                        <a href="img/1a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/1.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/2a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/2.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/3a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/3.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/4a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/4.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/5a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/5.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/6a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/6.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/7a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/7.jpg"alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/8a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/8.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/9a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/9.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <a href="img/10a.jpg" rel="prettyPhoto" title="Description of image">
+                                            <div>
+                                                <img src="img/10.jpg" alt="" />
+                                                <i></i>
+                                            </div>
+                                        </a>
+                                        <div class="gallery-tools">
+                                            <a href="confirmation_store.html"><i class="fa fa-shopping-cart"></i></a>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <div class="text-center">
+                                    <ul class="pagination pagination-bullet">
+                                        <li class="active"><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Main Content -->
